c80549ad1623eeb93b546e106f4890c2d7a9a4a8
LiZHao4 <1072745903@qq.com>
2024-10-13 13:50:22 +0800

    新增了设置个人信息的功能，用户可以设置性别、生日以及个人简介。 修改了用户ID的设定方式，新用户注册的ID将自动比当前用户最大的ID大1。 修改了用户信息修改的接口。

--- del.php
+++ del.php
@@ -8,6 +8,30 @@
 			$deleteStmt->bind_param("i", $userId);
 			$deleteStmt->execute();
 			if ($deleteStmt->affected_rows > 0) {
+				$avatarStmt = $dbConnection->prepare("SELECT avatar FROM users WHERE id = ?");
+				$avatarStmt->bind_param("i", $userId);
+				$avatarStmt->execute();
+				$res = $avatarStmt->get_result();
+				if ($res->num_rows > 0) {
+					$row = $res->fetch_assoc();
+					$avatar = $row['avatar'];
+				}
+				$avatarStmt->close();
+				if ($avatar == 1) {
+					$tokenStmt = $dbConnection->prepare("SELECT token FROM users WHERE id = ?");
+					$tokenStmt->bind_param("i", $userId);
+					$tokenStmt->execute();
+					$res2 = $tokenStmt->get_result();
+					if ($res2->num_rows > 0) {
+						$row2 = $res2->fetch_assoc();
+						$token = $row2['token'];
+					}
+					$tokenStmt->close();
+					$avatarPath = "avatar/" . $token . ".jpg";
+					if (file_exists($avatarPath)) {
+						unlink($avatarPath);
+					}
+				}
 				echo "success";
 			} else {
 				echo "fail";
