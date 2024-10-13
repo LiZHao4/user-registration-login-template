b58ce1c4858a36953904bbb7f7965e2c2b814aab
LiZHao4 <1072745903@qq.com>
2024-10-13 13:50:22 +0800

    新增了设置个人信息的功能，用户可以设置性别、生日以及个人简介。 修改了用户ID的设定方式，新用户注册的ID将自动比当前用户最大的ID大1。 修改了用户信息修改的接口。

--- save.php
+++ save.php
@@ -1,29 +1,47 @@
 <?php
-	if ($_SERVER["REQUEST_METHOD"] === "POST" && isset($_POST['id']) && isset($_POST['nick']) && isset($_POST['pass'])) {
+	if ($_SERVER["REQUEST_METHOD"] === "POST" && isset($_POST['id']) && isset($_POST['p'])) {
 		mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
 		try {
 			$databaseConnection = new mysqli("localhost", "yuancheng", "yc@123456", "yc");
-			$userId = (int)$_POST["id"];
-			$newNickname = $_POST["nick"];
-			$newPassword = $_POST["pass"];
-			$updateNicknameStmt = $databaseConnection->prepare("UPDATE users SET nick = ? WHERE id = ?");
-			$updateNicknameStmt->bind_param("si", $newNickname, $userId);
-			$updateNicknameStmt->execute();
+			$p = json_decode($_POST['p']);
+			$userId = (int)$_POST['id'];
+			$newNickname = isset($p->nick) ? $p->nick : null;
+			$newPassword = isset($p->password) ? $p->password : null;
+			$newGender = isset($p->gender) ? $p->gender : null;
+			$newBirth = isset($p->birth) ? $p->birth : null;
+			$newBio = isset($p->bio) ? $p->bio : null;
+			if ($newNickname) {
+				$st = $databaseConnection->prepare("UPDATE users SET nick = ? WHERE id = ?");
+				$st->bind_param("si", $newNickname, $userId);
+				$st->execute();
+				$st->close();
+			}
 			if ($newPassword) {
 				$hashedPassword = password_hash($newPassword, PASSWORD_DEFAULT);
-				$updatePasswordStmt = $databaseConnection->prepare("UPDATE users SET password = ? WHERE id = ?");
-				$updatePasswordStmt->bind_param("si", $hashedPassword, $userId);
-				$updatePasswordStmt->execute();
+				$st = $databaseConnection->prepare("UPDATE users SET password = ? WHERE id = ?");
+				$st->bind_param("si", $hashedPassword, $userId);
+				$st->execute();
+				$st->close();
 			}
-			if ($updateNicknameStmt->affected_rows > 0 || $newPassword && $updatePasswordStmt->affected_rows > 0) {
-				echo "success";
-			} else {
-				echo "fail";
+			if ($newGender) {
+				$st = $databaseConnection->prepare("UPDATE users SET gender = ? WHERE id = ?");
+				$st->bind_param("si", $newGender, $userId);
+				$st->execute();
+				$st->close();
 			}
-			$updateNicknameStmt->close();
-			if (isset($updatePasswordStmt)) {
-				$updatePasswordStmt->close();
+			if ($newBirth) {
+				$st = $databaseConnection->prepare("UPDATE users SET birth = ? WHERE id = ?");
+				$st->bind_param("si", $newBirth, $userId);
+				$st->execute();
+				$st->close();
 			}
+			if ($newBio) {
+				$st = $databaseConnection->prepare("UPDATE users SET bio = ? WHERE id = ?");
+				$st->bind_param("si", $newBio, $userId);
+				$st->execute();
+				$st->close();
+			}
+			echo "success";
 			$databaseConnection->close();
 		} catch (mysqli_sql_exception $sqlException) {
 			echo "fail";
