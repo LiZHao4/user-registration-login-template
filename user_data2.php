714adc114305e678e229a4ba9846d1efa1d46d5c
LiZHao4 <1072745903@qq.com>
2024-10-25 22:53:08 +0800

    增强了密码验证机制。 确保用户在不同设备上的登录状态同步且安全。 不再硬编码数据库配置信息。现在，数据库连接信息（如主机名、用户名、密码、数据库名和表名）被存储在一个外部配置文件中。

--- user_data2.php
+++ user_data2.php
@@ -1,11 +1,17 @@
 <?php
 	if ($_SERVER["REQUEST_METHOD"] === "POST" && isset($_POST['token'])) {
 		mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
+		$config = parse_ini_file('conf/settings.ini', true);
+		$host = $config['database']['host'];
+		$user = $config['database']['user'];
+		$pass = $config['database']['pass'];
+		$db = $config['database']['db'];
+		$table = $config['database']['table'];
 		try {
-			$databaseConnection = new mysqli("localhost", "yuancheng", "yc@123456", "yc");
+			$databaseConnection = new mysqli($host, $user, $pass, $db);
 			$postedToken = $_POST["token"];
 			if ($postedToken) {
-				$query = "SELECT id, user, nick, user_avatar, token, gender, birth, bio FROM users WHERE token = ?";
+				$query = "SELECT id, user, nick, user_avatar, token, gender, birth, bio FROM $table WHERE token = ?";
 				$statement = $databaseConnection->prepare($query);
 				$statement->bind_param("s", $postedToken);
 				$statement->execute();
