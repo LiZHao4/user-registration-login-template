f6f6cb088bb69780c75de4f71462dfe11d85ac4d
LiZHao4 <1072745903@qq.com>
2024-10-31 12:37:12 +0800

    系统新增多语言支持功能，现已支持繁体中文、英语、法语、俄语、西班牙语、阿拉伯语。

--- user_data.php
+++ user_data.php
@@ -1,4 +1,9 @@
 <?php
+	if (isset($_GET["lang"]) && file_exists("languages/" . $_GET["lang"] . ".json")) {
+		$language = json_decode(file_get_contents("languages/" . $_GET["lang"] . ".json"), true);
+	} else {
+		$language = json_decode(file_get_contents("languages/zh-CN.json"), true);
+	}
 	if (isset($_POST["v"]) && $_POST["v"] == "2") header('Content-Type: application/json');
 	else header('Content-Type: text/plain');
 	if ($_SERVER["REQUEST_METHOD"] === "POST" && isset($_POST['token'])) {
@@ -27,24 +32,24 @@
 							"nick" => $userData["nick"],
 							"avatar" => (bool)$userData["user_avatar"]
 						];
-						echo json_encode(["code" => 1, "msg" => "用户信息获取成功", "data" => $data]);
+						echo json_encode(["code" => 1, "msg" => $language["userInfoFetched"], "data" => $data]);
 					} else echo $userData["id"] . "\n" . $userData["user"] . "\n" . $userData["created_at"] . "\n" . $userData["token"] . "\n" . $userData["nick"] . "\n" . $userData["user_avatar"];
 				} else {
-					if (isset($_POST["v"]) && $_POST["v"] == "2") echo json_encode(["code" => 0, "msg" => "用户不存在或token无效，请重新登录"]);
+					if (isset($_POST["v"]) && $_POST["v"] == "2") echo json_encode(["code" => 0, "msg" => $language["invalidUserOrToken"]]);
 					else echo "relogin";
 				}
 				$preparedStatement->close();
 			} else {
-				if (isset($_POST["v"]) && $_POST["v"] == "2") echo json_encode(["code" => -1, "msg" => "缺少必要的token参数"]);
+				if (isset($_POST["v"]) && $_POST["v"] == "2") echo json_encode(["code" => -1, "msg" => $language["missingToken"]]);
 				else echo "fail";
 			}
 			$databaseConnection->close();
 		} catch (mysqli_sql_exception $sqlException) {
-			if (isset($_POST["v"]) && $_POST["v"] == "2") echo json_encode(["code" => -1, "msg" => "数据库查询出错：" . $sqlException->getMessage()]);
+			if (isset($_POST["v"]) && $_POST["v"] == "2") echo json_encode(["code" => -1, "msg" => $language["databaseError"]]);
 			else echo "fail";
 		}
 	} else {
-		if (isset($_POST["v"]) && $_POST["v"] == "2") echo json_encode(["code" => -1, "msg" => "无效的请求方法或缺少必要的token参数"]);
+		if (isset($_POST["v"]) && $_POST["v"] == "2") echo json_encode(["code" => -1, "msg" => $language["invalidRequest"]]);
 		else echo "fail";
 	}
 ?>
--- user_data2.php
+++ user_data2.php
@@ -1,4 +1,9 @@
 <?php
+	if (isset($_GET["lang"]) && file_exists("languages/" . $_GET["lang"] . ".json")) {
+		$language = json_decode(file_get_contents("languages/" . $_GET["lang"] . ".json"), true);
+	} else {
+		$language = json_decode(file_get_contents("languages/zh-CN.json"), true);
+	}
 	if (isset($_POST["v"]) && $_POST["v"] == "2") header('Content-Type: application/json');
 	else header('Content-Type: text/plain');
 	if ($_SERVER["REQUEST_METHOD"] === "POST" && isset($_POST['token'])) {
@@ -29,20 +34,20 @@
 						"birth" => $userData["birth"],
 						"bio" => $userData["bio"]
 					];
-					echo json_encode(["code" => 1, "msg" => "用户信息获取成功", "data" => $data]);
+					echo json_encode(["code" => 1, "msg" => $language["userInfoFetched"], "data" => $data]);
 				} else echo $userData["user"] . "\n" . $userData["nick"]. "\n" . $userData["id"] . "\n" . $userData["user_avatar"] . "\n" . $userData["token"] . "\n" . $userData["gender"] . "\n" . $userData["birth"] . "\n" . $userData["bio"];
 			} else {
-				if (isset($_POST["v"]) && $_POST["v"] == "2") echo json_encode(["code" => 0, "msg" => "用户不存在或token无效，请重新登录"]);
+				if (isset($_POST["v"]) && $_POST["v"] == "2") echo json_encode(["code" => 0, "msg" => $language["invalidUserOrToken"]]);
 				else echo "fail";
 			}
 			$statement->close();
 			$databaseConnection->close();
 		} catch (mysqli_sql_exception $sqlException) {
-			if (isset($_POST["v"]) && $_POST["v"] == "2") echo json_encode(["code" => -1, "msg" => "数据库查询出错：" . $sqlException->getMessage()]);
+			if (isset($_POST["v"]) && $_POST["v"] == "2") echo json_encode(["code" => -1, "msg" => $language["databaseError"]]);
 			else echo "fail";
 		}
 	} else {
-		if (isset($_POST["v"]) && $_POST["v"] == "2") echo json_encode(["code" => -1, "msg" => "无效的请求方法或缺少必要的token参数"]);
+		if (isset($_POST["v"]) && $_POST["v"] == "2") echo json_encode(["code" => -1, "msg" => $language["invalidRequest"]]);
 		else echo "fail";
 	}
 ?>
